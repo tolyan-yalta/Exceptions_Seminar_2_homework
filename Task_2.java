8f42a49b81bd0058cbd07c37713ccb6af09d4057
anatoly <tolyan.yalta@yandex.ru>
2023-04-24 22:17:48 +0300

    Add file Task_2.java

---
+++ Task_2.java
@@ -0,0 +1,26 @@
+// 2. Если необходимо, исправьте данный код (задание 2 
+// https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+
+// Оригинал задания:
+// try {
+//     int d = 0;
+//     double catchedRes1 = intArray[8] / d;
+//     System.out.println("catchedRes1 = " + catchedRes1);
+// } catch (ArithmeticException e) {
+//     System.out.println("Catching exception:" + e);
+// }
+
+
+
+class Task_2{
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        try {
+        int d = 0;
+        double catchedRes1 = intArray[8] / d;
+        System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException | ArrayIndexOutOfBoundsException | NullPointerException e) {
+            System.out.println("Catching exception:" + e);
+        } 
+    } 
+}
