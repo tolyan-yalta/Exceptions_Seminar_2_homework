b2accf78f95e4985c683c3fb5e41da0b945b62be
anatoly <tolyan.yalta@yandex.ru>
2023-04-24 23:26:51 +0300

    Change file Task_2.java

--- Task_2.java
+++ Task_2.java
@@ -15,8 +15,11 @@
 class Task_2{
     public static void main(String[] args) {
         int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //int[] intArray = {1, 2, 3, 4, 5, 6, 7};
+        //int[] intArray = null;
         try {
-        int d = 0;
+        //int d = 0;
+        int d = 2;
         double catchedRes1 = intArray[8] / d;
         System.out.println("catchedRes1 = " + catchedRes1);
         } catch (ArithmeticException | ArrayIndexOutOfBoundsException | NullPointerException e) {
